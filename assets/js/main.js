e4cb295512849bc94fee01e3e3f11bd90490837a
Nguyen Thi Ngoc Bich <93378230+NgocBich204@users.noreply.github.com>
2023-10-16 15:47:17 +0700

    Update:change the color of 3 boxes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
+/// doi mau js
+function changeColor() {
+    var box1 = document.getElementById("box1");
+    var box2 = document.getElementById("box2");
+    var box3 = document.getElementById("box3");
 
-// function changeColor() {
-//     var box1 = document.getElementById("box1");
-//     var box2 = document.getElementById("box2");
-//     var box3 = document.getElementById("box3");
+    var color1 = box1.style.backgroundColor;
+    var color2 = box2.style.backgroundColor;
+    var color3 = box3.style.backgroundColor;
 
-//     var color1 = box1.style.backgroundColor;
-//     var color2 = box2.style.backgroundColor;
-//     var color3 = box3.style.backgroundColor;
-
-//     box1.style.backgroundColor = color3;
-//     box2.style.backgroundColor = color1;
-//     box3.style.backgroundColor = color2;
-//   }
+    box1.style.backgroundColor = color3;
+    box2.style.backgroundColor = color1;
+    box3.style.backgroundColor = color2;
+  };
